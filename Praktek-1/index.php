f39cb0098a44f6b63aee7c0c257d9bb1e4a42d52
kikiaw <36694900+kikiaw@users.noreply.github.com>
2018-02-21 17:22:41 +0700

    Add files via upload

---
+++ Praktek-1/index.php
@@ -0,0 +1,5 @@
+<?php
+
+echo "<h1>Selamat Datang KIAW</h1>";
+
+?>
---
+++ Praktek-1/variabel.php
@@ -0,0 +1,16 @@
+<?php
+
+$nama = "pungki apri w";
+$usia = 23;
+$honor = 10.5;
+$statusMenikah = true;
+
+var_dump($nama); echo "<br>";
+var_dump($usia); echo "<br>";
+var_dump($honor); echo "<br>";
+var_dump($statusMenikah); echo "<br>";
+
+$statusMenikah = "Menikah";
+var_dump($statusMenikah); echo "<br>";
+
+?>
