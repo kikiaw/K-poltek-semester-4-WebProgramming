8b2034c995b48bde18ab7f4e2fe85f16766d58f4
kikiaw <36694900+kikiaw@users.noreply.github.com>
2018-02-28 16:44:19 +0700

    Add files via upload

---
+++ Praktek-2/array-multi.php
@@ -0,0 +1,18 @@
+<?php
+$data = array(
+    "web.programming" => array(
+        "pungkyaw" => 9,
+        "ucup" => 8,
+        "rifai" => 7,
+    ),
+    "kalkulus" => array(
+        "ucup" => 9,
+        "ridai" => 8,
+        "pungkyaw" => 7,
+    )
+);
+
+echo "Nilai web.programming untuk pungkyaw : ".$data["web.programming"]["pungkyaw"]."<br>";
+echo "Nilai Kalkulus untuk pungkyaw : ".$data["kalkulus"]["pungkyaw"];
+
+?>
