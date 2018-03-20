16fc82550ab6073d6cb20d1f4092b078a516a119
kikiaw <36694900+kikiaw@users.noreply.github.com>
2018-03-20 15:59:51 +0700

    Add files via upload

---
+++ stok-app/coba.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+    <title>Penggunaan Tag Image</title>
+    </head>
+    <body>
+    <h1>Everything About You</h1>
+    <img src="yani.jpg" />
+    </body>
+    </html>
